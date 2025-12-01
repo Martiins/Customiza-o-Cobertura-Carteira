ba215013887e410a93087a72e7c6381e91c16a30
Zeev <bc15c2@gmail.com>
2025-12-01 18:09:31 -0300

    Update customizacao.js

--- customizacao.js
+++ customizacao.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     function aplicarCoresClientes() {
-        const tabela = document.querySelector('table[data-groupid="4881"]');
+        const tabela = document.querySelector('table[data-groupid="6484"]');
         if (!tabela) return;
 
         const linhas = tabela.querySelectorAll('tbody tr:not(.header)');
 
         linhas.forEach(tr => {
-            const campo = tr.querySelector('input[name="inp22028"]');
+            const campo = tr.querySelector('input[name="inp28249"]');
             if (!campo) return;
 
             const valor = campo.value.trim();
@@ -29,10 +29,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Se o usuário mudar o campo no select (caso existam selects) — opcional
     document.addEventListener("change", function (e) {
-        if (e.target && e.target.name === "inp22009") {
+        if (e.target && e.target.name === "inp28249") {
             aplicarCoresClientes();
         }
     });
 
 });
+
 
